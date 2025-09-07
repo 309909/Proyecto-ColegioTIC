812a4458afbb7783f41ae2565ea833e28d2561d1
309909 <manuel.educacion25@gmail.com>
2025-09-07 18:40:12 -0300

    Subiendo proyecto completo

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Navbar scroll effect
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
+  // Inicialización de tooltips
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
